4cf3b7bb817b4985d9af1c7e0cc7cd6535b09b59
gzvidal <vidal.gonza23@gmail.com>
2020-08-10 09:04:48 -0300

    Primer ejercicio hecho

--- uno.js
+++ uno.js
@@ -9,5 +9,72 @@ pedir datos por prompt y mostrar por document.write o console.log
 */
 function mostrar()
 {
-	alert("uno");
+	//declarar variables 
+
+	var nombre;
+	var temperatura;
+	var sexo;
+	var edad; 
+	var contador;
+	var cantidadMujeres = 0;
+	var cantidadHombres = 0;
+	var contadorPersonas = 0;
+	var edadTotal = 0;
+	var edadPromedio;
+	var flagTemperatura = 0;
+	var temperaturaMaxima;
+	var nombreMujerConTemperatura;
+
+	//tomar datos 
+
+	for(contador = 0; contador < 3 ; contador++){
+
+		nombre = prompt("Ingrese el nombre del paciente"); 
+
+		temperatura = parseFloat(prompt("Ingrese la temperatura del paciente"));
+		while(isNaN(temperatura)){
+			temperatura = parseFloat(prompt("Error. Ingrese una temperatura válida"));
+		}
+
+		sexo = prompt("Ingrese el sexo del paciente");
+		while(sexo != "f" && sexo != "m"){
+			sexo = prompt("Error. Por favor, ingrese 'f' para femenino o 'm' para masculino.'");
+		}
+		
+		edad = parseInt(prompt("Ingrese la edad del paciente"));
+		while(isNaN(edad)){
+			edad = parseInt(prompt("Error. Ingrese la edad del paciente"));
+		}
+
+		switch(sexo){
+			case "f":
+			contadorPersonas++;
+			cantidadMujeres++;
+			if(flagTemperatura == 0 || temperaturaMaxima < temperatura){
+				flagTemperatura = 1;
+				temperaturaMaxima = temperatura;
+				nombreMujerConTemperatura = nombre;
+			}
+			break;
+			case "m":
+			contadorPersonas++;
+			cantidadHombres++;
+			break;
+		}
+
+		edadTotal = edadTotal + edad;
+	}
+	
+	edadPromedio = edadTotal / contadorPersonas;
+
+	//Punto A: 
+	console.log("Hombres: " + cantidadHombres + ". Mujeres: " + cantidadMujeres);
+	//Punto B: 
+	console.log("El promedio de edad total es: " + edadPromedio);
+	//Punto C: 
+	if(cantidadMujeres == 0){
+		console.log("No se ingresó ninguna mujer");
+	} else {
+		console.log("La mujer con más temperatura se llama: " + nombreMujerConTemperatura);
+	}
 }
